02d061e3cbac3e4b34ece2e9c80ec70e57c33c7b
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-11-28 23:07:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let initValue = 0;
+
+const totalBatteries = batteryBatches.reduce((accumulator, currValue) => accumulator + currValue, initValue);
